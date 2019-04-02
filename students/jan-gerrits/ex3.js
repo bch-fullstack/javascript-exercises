098c779cec1474fec3d0fbe96f9a7b95149d02b1
Gerrits Jan <s1900516@edu.bc.fi>
2019-04-02 14:09:15 +0300

    add exercise files

---
+++ students/jan-gerrits/ex3.js
@@ -0,0 +1,21 @@
+function joinArray(input) {
+    var index = 0,
+    result = '';
+
+    while(index < input.length)
+    result = result + ' ' + input[index];
+    index++;
+}
+
+return result;
+}
+/*
+Write a simple JavaScript program to join all elements of the following array into a string.
+
+Sample array : 
+var myText = ['This', 'is', 'not', 'working'];
+joinArray(myText);
+
+Expected Output : 
+This is not working
+ */
