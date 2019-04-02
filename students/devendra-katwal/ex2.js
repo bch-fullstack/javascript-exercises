650e004525d8448b6114fb0806d36c0e64e94634
Katwal Devendra <s1900526@edu.bc.fi>
2019-04-02 14:33:36 +0300

    array answer

--- students/devendra-katwal/ex2.js
+++ students/devendra-katwal/ex2.js
@@ -9,3 +9,21 @@ console.log(first([7, 9, 0, -2],6)); // [7, 9, 0, -2]
 console.log(first([7, 9, 0, -2],-3)); // [] 
 
 */
+
+function first(arr, n){
+    if(arr.length === 0){
+        return []
+    }else if (arguments.length>1){
+        return arr.splice(0,n)
+    }else if (arr.length ===1){
+        return arr
+    }else if (n === null || n === 'undefined'){
+        return arr[0]
+    }
+}
+console.log(first([7, 9, 0, -2])); // 7
+console.log(first([],3)); // []
+console.log(first([7, 9, 0, -2],3)); // [7,9,0]
+console.log(first([7, 9, 0, -2],6)); // [7, 9, 0, -2]
+console.log(first([7, 9, 0, -2],-3)); // [] 
+console.log(first([7, 9, 0, -2],'undefined')); // [] 
