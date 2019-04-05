436cee8c9998d953d5df382cfbeb85cc18f357fc
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-05 10:45:26 +0300

    add jquery injection and css

--- students/sample-page/js/scripts.js
+++ students/sample-page/js/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 console.log('This is the line of code that is ran first when the page loads');
 console.log('This is the second line of code that is ran when the page loads');
 
@@ -14,9 +17,12 @@ sampleFunction(); // we are invoking the function right here
 function nextFunction() {
     var i = 10;
     console.log('This is the value of i right before the while line below');
+    console.log('This is the value of i right before the while line below');
+
     while (i--) {
         console.log('When you see this line, the value of i has just been subtracted by 1');       
         console.log('The value of i is currently: ' + i);
     }
     console.log('One this line is reached, the value of i is: ' + 0)
 }
+
