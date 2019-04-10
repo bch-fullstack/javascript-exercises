17b5fe13f4717cde69052ab4665254b730cfcca0
Douglas Locklin <douglas.locklin@gmail.com>
2019-04-10 13:57:14 +0300

    my clocks are working, need some styles

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -9,22 +9,32 @@ function displayDigitalTime (){
     digitalDisplay.innerHTML = 'The time now is: ' + hr + ':' + min + ':'+ sec;
 }
 
-var HOURHAND = document.querySelector("#hour");
-var MINUTEHAND = document.querySelector("#minute");
-var SECONDHAND = document.querySelector("#second"); 
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var now = new Date(),
+        hr = now.getHours(),
+        min = now.getMinutes(),
+        seconds = now.getSeconds();
 
-setInterval (displayAnalogTime, 1000);
-function displayAnalogTime (){
-        var now = new Date();
-        var hr = now.getHours();
-        var min = now.getMinutes();
-        var sec = now.getSeconds();
+    var secondsAngle = seconds*6;
+    var minAngle = min*6 + seconds/60 * 6;
+    var hourAngle = hr*30 + min/60 * 30 + seconds/3600 * 30;
 
-        var secAngle = sec * 6;
-        var minAngle = min * 6 + sec/60 * 6;
-        var hrAngle = hr * 30 + min/60 * 30 + sec/3600 * 30;
-    
-        document.getElementById('hour').style.transform = 'rotate(' + hrAngle + 'deg)';
-        document.getElementById('minute').style.transform = 'rotate(' + minAngle + 'deg)';
-        document.getElementById('second').style.transform = 'rotate(' + secAngle + 'deg)';
-}
+    adjustArms(hourAngle, minAngle, secondsAngle);
+
+    function updateArmsLocation() {
+        secondsAngle += 6;
+        minAngle += 0.1;
+        hourAngle += 1/120;
+
+        adjustArms(hourAngle, minAngle, secondsAngle);
+    }
+
+    function adjustArms(a,b,c) {
+        document.getElementById('hour').style.transform = 'rotate(' + a +'deg)';
+        document.getElementById('minute').style.transform = 'rotate(' + b +'deg)';
+        document.getElementById('second').style.transform = 'rotate(' + c +'deg)';
+    }
+
+    setInterval(updateArmsLocation, 1000);
+
+});
