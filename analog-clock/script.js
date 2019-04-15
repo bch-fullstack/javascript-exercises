3a2711c05ba4fb0de64748b1dbbef3b81c0151b6
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-04-15 22:39:44 +0300

    update algorithm

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -1,31 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
     var now = new Date(),
         hr = now.getHours(),
         min = now.getMinutes(),
         seconds = now.getSeconds();
 
-    var secondsAngle = seconds*6;
-    var minAngle = min*6 + seconds/60 * 6;
-    var hourAngle = hr*30 + min/60 * 30 + seconds/3600 * 30;
+    var secondsAngle = seconds * 6;
+    var minAngle = min * 6 + seconds / 60 * 6;
+    var hourAngle = hr * 30 + min / 60 * 30 + seconds / 3600 * 30;
 
     adjustArms(hourAngle, minAngle, secondsAngle);
 
     function updateArmsLocation() {
         secondsAngle += 6;
         minAngle += 0.1;
-        hourAngle += 1/120;
+        hourAngle += 1 / 120;
 
         adjustArms(hourAngle, minAngle, secondsAngle);
     }
 
-    function adjustArms(a,b,c) {
-        document.getElementById('hour').style.transform = 'rotate(' + a +'deg)';
-        document.getElementById('minute').style.transform = 'rotate(' + b +'deg)';
-        document.getElementById('second').style.transform = 'rotate(' + c +'deg)';
+    function adjustArms(a, b, c) {
+        document.getElementById('hour').style.transform = 'rotate(' + a + 'deg)';
+        document.getElementById('minute').style.transform = 'rotate(' + b + 'deg)';
+        document.getElementById('second').style.transform = 'rotate(' + c + 'deg)';
     }
 
     setInterval(updateArmsLocation, 1000);
-
+   
 });
-
-
