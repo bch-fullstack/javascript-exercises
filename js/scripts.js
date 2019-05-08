51d9b0599ae15ed877bf8d996fdd75ad3bdf941c
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-05-08 14:02:00 +0300

    OOP approach of flickr exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,71 @@
-var flickrAPI = '//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+var createFlickrSearcher = {
+    flickrAPI: null, // api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?
+    flickrLinkEl: null,
+    flickrTitle: null,
+    imageWrapperEl: null,
+    searchTermEl: null,
+    searchButtonEl: null,
+    renderContent: function (resp) {
+        this.flickrLinkEl.text(resp.link);
+        this.flickrTitle.text(resp.title);
+        this.imageWrapperEl.html(''); // clear out previous search result
 
-function handleResponse(resp) {
-    console.log('The request has been completed'); 
-    console.log(resp);
+        for (var i = 0; i < resp.items.length; i++) {
+            this.createImage(resp.items[i].media.m);
+        }
+    },
+    createImage: function (url) {
+        this.imageWrapperEl.append('<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="' + url + '"></img></div>');
+    },
+    handleResponse: function (resp) {
+        console.log('The request has been completed');
+        console.log(resp);
 
-    if (resp.items.length === 0) {
-        $('#imageWrapper').text('There is no image matching your search term');
-        return;
+        if (resp.items.length === 0) {
+            this.imageWrapperEl.text('There is no image matching your search term');
+            return;
+        }
+
+        this.renderContent(resp);
+    },
+    doAjaxCall: function (searchTerm) {
+        var paramsForFlickr = {
+            tags: searchTerm,
+            tagmode: 'any',
+            format: 'json'
+        };
+    
+        $.getJSON(
+            this.flickrAPI,
+            paramsForFlickr,
+            this.handleResponse.bind(this)
+        );
+    },
+    init: function (api, link, title, wrapper, searchTerm, searchButton) {
+        var scope = this;
+
+        this.flickrLinkEl = $(link);
+        this.flickrTitle = $(title);
+        this.imageWrapperEl = $(wrapper);
+        this.searchTermEl = $(searchTerm);
+        this.searchButtonEl = $(searchButton);
+        this.flickrAPI = api;
+
+        this.searchButtonEl.on('click', function () {
+            var searchTerm = scope.searchTermEl.val();
+            console.log(searchTerm);
+            scope.doAjaxCall(searchTerm);
+        });
+
+        this.searchTermEl.on('change', function () {
+            var searchTerm = scope.searchTermEl.val();
+            console.log(searchTerm);
+            scope.doAjaxCall(searchTerm);
+        });
+
     }
-
-    renderContent(resp);
-}
-
-function renderContent(resp) {
-    $('#flickrLink').text(resp.link);
-    $('#flickrTitle').text(resp.title);
-    $('#imageWrapper').html(''); // clear out previous search result
-
-    for (var i = 0; i < resp.items.length; i++) {
-        createImage(resp.items[i].media.m);
-    }
-}
-
-function createImage(url) {
-    $('#imageWrapper').append('<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="' + url + '"></img></div>');
-}
-
-function doAjaxCall(searchTerm) {
-    var paramsForFlickr = {
-        tags: searchTerm,
-        tagmode: 'any',
-        format: 'json'
-    };
-
-    $.getJSON( 
-        flickrAPI, 
-        paramsForFlickr,
-        handleResponse
-    );
-}
+};
 
 $(document).ready(function(){
-    $('#search').on('click', function(){
-        var searchTerm = $('#searchTerm').val();
-        console.log(searchTerm);
-        doAjaxCall(searchTerm);
-    });
-
-    $('#searchTerm').on('change', function(){
-        var searchTerm = $('#searchTerm').val();
-        console.log(searchTerm);
-        doAjaxCall(searchTerm);
-    });
+    var flickrSampleSearch = createFlickrSearcher.init('//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', '#flickrLink', '#flickrTitle', '#imageWrapper', '#searchTerm', '#search');
 });
-
-
