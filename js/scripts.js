678564af823fabcfb8491a6b7b64eb43b3b59a0e
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-05-08 13:07:23 +0300

    add styling for search result

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
 var flickrAPI = '//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 
-var paramsForFlickr = {
-    tags: 'pasila',
-    tagmode: 'any',
-    format: 'json'
-};
-
 function handleResponse(resp) {
     console.log('The request has been completed'); 
     console.log(resp);
+
+    if (resp.items.length === 0) {
+        $('#imageWrapper').text('There is no image matching your search term');
+        return;
+    }
+
     renderContent(resp);
 }
 
 function renderContent(resp) {
     $('#flickrLink').text(resp.link);
     $('#flickrTitle').text(resp.title);
+    $('#imageWrapper').html(''); // clear out previous search result
 
     for (var i = 0; i < resp.items.length; i++) {
-        $('#imageWrapper').append('<img src="' + resp.items[i].media.m + '"></img>');
+        createImage(resp.items[i].media.m);
     }
 }
 
-function doAjaxCall() {
+function createImage(url) {
+    $('#imageWrapper').append('<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="' + url + '"></img></div>');
+}
+
+function doAjaxCall(searchTerm) {
+    var paramsForFlickr = {
+        tags: searchTerm,
+        tagmode: 'any',
+        format: 'json'
+    };
+
     $.getJSON( 
         flickrAPI, 
         paramsForFlickr,
@@ -29,4 +40,18 @@ function doAjaxCall() {
     );
 }
 
-doAjaxCall();
+$(document).ready(function(){
+    $('#search').on('click', function(){
+        var searchTerm = $('#searchTerm').val();
+        console.log(searchTerm);
+        doAjaxCall(searchTerm);
+    });
+
+    $('#searchTerm').on('change', function(){
+        var searchTerm = $('#searchTerm').val();
+        console.log(searchTerm);
+        doAjaxCall(searchTerm);
+    });
+});
+
+
