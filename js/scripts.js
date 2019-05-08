b25ff6ba1786cae53c8067fc89687aac7a4a4664
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-05-08 10:59:49 +0300

    refactor code and append image links to markup

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
 var flickrAPI = '//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 
-$.getJSON( flickrAPI, {
+var paramsForFlickr = {
     tags: 'pasila',
     tagmode: 'any',
     format: 'json'
-    },
-    function(data){
-        console.log(data);
+};
+
+function handleResponse(resp) {
+    console.log('The request has been completed'); 
+    console.log(resp);
+    renderContent(resp);
+}
+
+function renderContent(resp) {
+    $('#flickrLink').text(resp.link);
+    $('#flickrTitle').text(resp.title);
+    
+    var imageItems = resp.items,
+        index = resp.items.length;
+
+    for (var i = 0; i < resp.items.length; i++) {
+        $('#imageWrapper').append('<div>' + resp.items[i].link + '</div>');
     }
-);
+
+}
+
+function doAjaxCall() {
+    $.getJSON( 
+        flickrAPI, 
+        paramsForFlickr,
+        handleResponse
+    );
+}
+
+doAjaxCall();
